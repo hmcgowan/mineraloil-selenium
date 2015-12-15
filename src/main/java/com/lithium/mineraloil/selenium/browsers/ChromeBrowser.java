58c6705546fbf722fc9684abc70cddb86b7e49d7
Donald Johnstone <donald.johnstone@lithium.com>
2015-12-15 15:04:05 -0600

    Add a download directory to Chrome on grid

--- src/main/java/com/lithium/mineraloil/selenium/browsers/ChromeBrowser.java
+++ src/main/java/com/lithium/mineraloil/selenium/browsers/ChromeBrowser.java
@@ -33,7 +33,7 @@ public class ChromeBrowser extends BrowserImpl {
         return new ChromeDriver(getProfile());
     }
 
-    private DesiredCapabilities getProfile(){
+    private DesiredCapabilities getProfile() {
         Map<String, Object> prefs = new HashMap<>();
         DesiredCapabilities profile = DesiredCapabilities.chrome();
         ChromeOptions options = new ChromeOptions();
@@ -42,8 +42,8 @@ public class ChromeBrowser extends BrowserImpl {
         downloadDirectory = String.format("%s", dataDirectory + "/Downloads");
 
         prefs.put("download.default_directory", downloadDirectory);
-		prefs.put("profile.default_content_settings.popups", 0);
-		options.setExperimentalOption("prefs", prefs);
+        prefs.put("profile.default_content_settings.popups", 0);
+        options.setExperimentalOption("prefs", prefs);
         profile.setCapability("name", "chrome");
         profile.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
         profile.setCapability("chrome.binary", chromePath.getFile());
